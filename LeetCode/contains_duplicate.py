df9b8c9e71242b65e8332399126724258831ff62
Cankutay Mutlu <cankutaymutlu21@gmail.com>
2025-09-14 16:25:01 +0300

    LeetCode 'Contains Duplicate' Problem Solution

---
+++ LeetCode/contains_duplicate.py
@@ -0,0 +1,9 @@
+# LeetCode Contains Duplicates Problem 
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        if len(set(nums)) == len(nums):
+            return False
+        else:
+            return True
+        
