3866811175a479d74217d27c11220934e3ef7605
Cankutay Mutlu <cankutaymutlu21@gmail.com>
2025-09-26 08:28:12 +0300

    Added LeetCode Remove Element Solution

---
+++ LeetCode/remove_element.py
@@ -0,0 +1,10 @@
+# LeetCode '27. Remove Element' Problem
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        k = 0
+        for num in nums:
+            if num != val:
+                nums[k] = num
+                k += 1
+        return k
