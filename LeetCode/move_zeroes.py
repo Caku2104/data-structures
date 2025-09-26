c0fcd5b12c1a192e926e1734fe2759fc44ee19f8
Cankutay Mutlu <cankutaymutlu21@gmail.com>
2025-09-26 08:38:47 +0300

    Added LeetCode 283. Move Zeroes Solution

---
+++ LeetCode/move_zeroes.py
@@ -0,0 +1,11 @@
+# LeetCode '283. Move Zeroes' Problem
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        k = 0
+        for num in nums:
+            if num != 0:
+                nums[k] = num
+                k += 1
+        for i in range(k, len(nums)):
+            nums[i] = 0
